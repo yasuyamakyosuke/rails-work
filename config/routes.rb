0c3f1b17a474f05561ad89edbcc71091b3328cef
泰山恭輔 <taizankyousuke@taizankyousukenoMacBook-Air.local>
2020-03-09 20:31:27 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'todolists/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ get 'top' => 'homes#top'
+
+ post 'todolists' => 'todolists#create'
+
+
+ get 'todolists' => 'todolists#index'
+
+ get 'todolists/:id' => 'todolists#show', as: 'todolist'
+      #.../todolists/1や.../todolists/3に該当する
+
+
+end
