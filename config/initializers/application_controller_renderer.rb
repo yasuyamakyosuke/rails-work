d833a47264e26d4db6a7f5c75ceb998bf76ed791
泰山恭輔 <taizankyousuke@taizankyousukenoMacBook-Air.local>
2020-03-11 21:37:20 +0900

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd70f77d10e2dab22f51ffbca73e31cf9a6bd5365e822391074a3cd7509fdc1c46e245ceef8926025420d14fd3226718e05882cad630c1dcf75f83ff281a7d9c5'
